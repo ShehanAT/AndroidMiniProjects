392f847e15de94bd52411d71120c5b33a37b73bd
Nipun <nipun.atukorala@gmail.com>
2021-08-07 11:58:21 -0400

    added RadioPlayer activity to RadioPlayerApp

--- RadioPlayerApp/app/src/main/java/com/coding/informer/radioplayerapp/MainActivity.java
+++ RadioPlayerApp/app/src/main/java/com/coding/informer/radioplayerapp/MainActivity.java
@@ -2,7 +2,11 @@ package com.coding.informer.radioplayerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.coding.informer.radioplayerapp.activities.RadioPlayerActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void onButtonClick(View view){
+        Intent radioIntent = new Intent(MainActivity.this, RadioPlayerActivity.class);
+        startActivity(radioIntent);
     }
 }
---
+++ RadioPlayerApp/app/src/main/java/com/coding/informer/radioplayerapp/activities/RadioPlayerActivity.java
@@ -0,0 +1,16 @@
+package com.coding.informer.radioplayerapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.coding.informer.radioplayerapp.R;
+
+public class RadioPlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_radio_player);
+    }
+}
