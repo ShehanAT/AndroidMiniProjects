35135fca2fe60e1ab64c2d1bd52f21d8b4e61144
Nipun <shehanatuk@gmail.com>
2021-11-22 11:41:44 -0500

    Added untracked files

---
+++ RoomManualMigrationExample/app/src/main/java/com/coding/informer/roommanualmigrationexample/MainActivity.java
@@ -0,0 +1,42 @@
+package com.coding.informer.roommanualmigrationexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.coding.informer.roommanualmigrationexample.dao.VehicleDAO;
+import com.coding.informer.roommanualmigrationexample.database.AppDatabase;
+import com.coding.informer.roommanualmigrationexample.models.Vehicle;
+
+import java.util.List;
+
+import static com.coding.informer.roommanualmigrationexample.database.migrations.Migration1.MIGRATION_1_2;
+import static com.coding.informer.roommanualmigrationexample.database.migrations.Migration1.MIGRATION_2_3;
+import static com.coding.informer.roommanualmigrationexample.database.migrations.Migration1.MIGRATION_3_4;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        AsyncTask.execute(() -> {
+            AppDatabase db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "example_db")
+                    .addMigrations(MIGRATION_1_2, MIGRATION_2_3, MIGRATION_3_4).build();
+
+//            Vehicle vehicle = new Vehicle();
+//            vehicle.setName("Civic");
+//
+//            VehicleDAO vehicleDAO = db.getVehicleDAO();
+//            vehicleDAO.insertAll(vehicle);
+//            List<Vehicle> savedVehicles = vehicleDAO.getAll();
+//            Log.d("savedVehicles size: ", "" + savedVehicles.size());
+        });
+        Toast.makeText(getApplicationContext(), "Database working?", Toast.LENGTH_LONG);
+    }
+}
