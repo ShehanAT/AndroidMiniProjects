43b00136a3583ebcb786dedf0c870d54cb24de9d
Nipun <shehanatuk@gmail.com>
2021-11-24 10:28:27 -0500

    Modified Migration1.java file

--- RoomManualMigrationExample/app/src/main/java/com/coding/informer/roommanualmigrationexample/database/migrations/Migration1.java
+++ RoomManualMigrationExample/app/src/main/java/com/coding/informer/roommanualmigrationexample/database/migrations/Migration1.java
@@ -8,7 +8,7 @@ public class Migration1 {
     public static final Migration MIGRATION_1_2 = new Migration(1, 2) {
         @Override
         public void migrate(SupportSQLiteDatabase database) {
-            database.execSQL("CREATE TABLE `Vehicle` (`id` INTEGER NOT NULL, "
+            database.execSQL("CREATE TABLE `vehicle` (`id` INTEGER NOT NULL, "
                     + "`name` TEXT NOT NULL, PRIMARY KEY(`id`))");
         }
     };
@@ -16,15 +16,15 @@ public class Migration1 {
     public static final Migration MIGRATION_2_3 = new Migration(2, 3) {
         @Override
         public void migrate(SupportSQLiteDatabase database) {
-            database.execSQL("ALTER TABLE `Vehicle` ADD COLUMN `model` INTEGER NOT NULL DEFAULT 0");
+            database.execSQL("ALTER TABLE `vehicle` ADD COLUMN `model` INTEGER NOT NULL DEFAULT 0");
         }
     };
 
     public static final Migration MIGRATION_3_4 = new Migration(3, 4) {
         @Override
         public void migrate(SupportSQLiteDatabase database) {
-            database.execSQL("DROP TABLE `Vehicle`");
-            database.execSQL("CREATE TABLE `Vehicle` (`id` INTEGER NOT NULL, `name` TEXT NOT NULL, PRIMARY KEY(`id`))");
+            database.execSQL("DROP TABLE `vehicle`");
+            database.execSQL("CREATE TABLE `vehicle` (`id` INTEGER NOT NULL, `name` TEXT NOT NULL, PRIMARY KEY(`id`))");
         }
     };
 
