d78c2c4dbb6ddfe5f943385dbd25e656fa424670
Nipun <shehanatuk@gmail.com>
2021-11-27 17:30:13 -0500

    Added activity_login, activity_register resource files

---
+++ AndroidLoginRegistrationExample/app/src/main/java/com/coding/informer/androidloginregistrationexample/activities/LoginActivity.java
@@ -0,0 +1,9 @@
+package com.coding.informer.androidloginregistrationexample.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = LoginActivity.class.getSimpleName();
+
+}
---
+++ AndroidLoginRegistrationExample/app/src/main/java/com/coding/informer/androidloginregistrationexample/activities/RegisterActivity.java
@@ -0,0 +1,10 @@
+package com.coding.informer.androidloginregistrationexample.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private static final String TAG = RegisterActivity.class.getSimpleName();
+
+
+}
