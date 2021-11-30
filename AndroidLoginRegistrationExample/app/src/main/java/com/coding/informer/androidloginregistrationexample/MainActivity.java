605b04cc4af873680eb6e7c78ae375b792ae2d47
Nipun <shehanatuk@gmail.com>
2021-11-30 14:27:33 -0500

    Added unversioned files to source control

---
+++ AndroidLoginRegistrationExample/app/src/main/java/com/coding/informer/androidloginregistrationexample/MainActivity.java
@@ -0,0 +1,45 @@
+package com.coding.informer.androidloginregistrationexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.coding.informer.androidloginregistrationexample.activities.LoginActivity;
+import com.coding.informer.androidloginregistrationexample.activities.RegisterActivity;
+import com.coding.informer.androidloginregistrationexample.database.AppDatabase;
+import com.coding.informer.androidloginregistrationexample.helpers.DatabaseHelper;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button loginBtn;
+    private Button signUpBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loginBtn = findViewById(R.id.login_btn);
+        signUpBtn = findViewById(R.id.sign_up_btn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+           public void onClick(View v){
+               Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+               startActivity(intent);
+           }
+        });
+
+        DatabaseHelper.createDatabase(getApplicationContext());
+
+    }
+}
