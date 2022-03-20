fb084b0dd9955fb8a55b987fc6984091e8702119
Nipun <shehanatuk@gmail.com>
2022-03-19 20:35:48 -0400

    added files from simple_material_app

--- simple_material_app/lib/utils/database-service.dart
+++ simple_material_app/lib/utils/database-service.dart
@@ -4,4 +4,18 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:simple_material_app/models/food-track-entry.dart';
 
-class DatabaseService {}
+class DatabaseService {
+  DatabaseService();
+
+  DatabaseReference _foodTrackRef =
+      FirebaseDatabase.instance.ref().child("foodTrack");
+  final DateTime today =
+      DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day);
+
+  Future addFoodTrackData(FoodTrackEntry foodTrackEntry) async {
+    return await _foodTrackRef.push().set({
+      'calories': foodTrackEntry.calories,
+      'createdOn': foodTrackEntry.date.toString()
+    });
+  }
+}
